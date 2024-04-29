a7baa42d5be52fbabde3afa135eb015c35264075
Nicholas Di Pasquali <119329699+NikoDP20@users.noreply.github.com>
2024-04-29 22:47:15 +0100

    updated course section with labels and individual pages

---
+++ js-/coursePDE.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Implement the logic for returning to the home page
+    document.getElementById('return-home').addEventListener('click', function() {
+        window.location.href = '../index.html'; // Redirect to the main page or index page
+    });
+    
+    // Add event listener to the "Learn More" button
+    document.getElementById('learn-more').addEventListener('click', function() {
+        // Redirect to the specified link
+        window.location.href = 'https://www.canterbury.ac.uk/study-here/courses/product-design-engineering-beng';
+    });
+});
+
