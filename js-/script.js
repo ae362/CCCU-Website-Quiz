dc2f84466d24c8f0604d3f00825bc7316c175e12
Nicholas Di Pasquali <119329699+NikoDP20@users.noreply.github.com>
2024-05-01 17:34:35 +0100

    seperated follow us section

---
+++ js-/script.js
@@ -0,0 +1,7 @@
+document.getElementById("instagrambutton").addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/canterburyccuni?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
+});
+
+document.getElementById("AnswersDayButton").addEventListener("click", function() {
+    window.location.href = "https://www.canterbury.ac.uk/campaign/answers-day";
+});
